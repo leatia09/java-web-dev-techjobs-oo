cbc7e7844a5151ccd2e40dc66f2a9f7c1d3ab952
Leatia Stevenson <leatia09@icloud.com>
2019-11-26 21:47:36 -0600

    first commit

--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs_oo;
 
+import java.util.Objects;
+
 public class PositionType {
     private int id;
     private static int nextId = 1;
@@ -14,6 +16,20 @@ public class PositionType {
         this();
         this.value = value;
     }
+
+    @Override
+    public String toString() { return value; }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(!(o instanceof PositionType)) return false;
+        PositionType that = (PositionType) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() { return Objects.hash(id); }
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
