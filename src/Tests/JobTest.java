e29e94692535915c939af37dceb55c27382163f2
Leatia Stevenson <leatia09@icloud.com>
2019-12-01 22:33:34 -0600

    Step 4 complete

---
+++ src/Tests/JobTest.java
@@ -0,0 +1,59 @@
+package Tests;
+
+import org.launchcode.techjobs_oo.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+
+
+public class JobTest {
+
+    Job test_job1;
+    Job test_job2;
+    Job test_job3;
+    Job test_job4;
+    Job test_job5;
+    Job test_job6;
+
+
+    @Before
+    public void createJobObject() {
+        test_job1 = new Job();
+        test_job2 = new Job();
+        test_job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        test_job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        test_job5 = new Job("", new Employer("ACME"), new Location("City"), new PositionType("Developer"), new CoreCompetency("Persistence"));
+        test_job6 = new Job("", new Employer("ACME"), new Location("City"), new PositionType("Developer"), new CoreCompetency("Persistence"));
+    }
+
+    @Test
+    public void testSettingJobID() {
+        assertEquals(test_job1.getId(), test_job2.getId(), 1);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        assertTrue( test_job3 instanceof Job );
+        assertTrue( test_job3.getEmployer() instanceof Employer );
+        assertTrue( test_job3.getLocation() instanceof Location );
+        assertTrue( test_job3.getPositionType() instanceof PositionType );
+        assertTrue( test_job3.getCoreCompetency() instanceof CoreCompetency );
+
+        assertEquals( "Product tester", test_job3.getName());
+        assertEquals( "ACME", test_job3.getEmployer().getValue());
+        assertEquals( "Desert", test_job3.getLocation().getValue());
+        assertEquals( "Quality control", test_job3.getPositionType().getValue());
+        assertEquals( "Persistence", test_job3.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        int firstId = test_job3.getId();
+        int secondId = test_job4.getId();
+        assertFalse( firstId == secondId);
+    }
+}
